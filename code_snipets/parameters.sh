99a4e45f396c44ddf1315dee76b06f56e65a3fd3
Jeevan Venkataramana <Jeevan Venkataramana>
2019-03-09 02:55:10 +0000

    updated

---
+++ code_snipets/parameters.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+FIRST=$1
+LAST=$2
+echo "My Name is $1 , $2"
---
+++ code_snipets/variable.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+FIRST_NAME=Jeevan
+LAST_NAME="Venkataramana"
+DOB="10 JAN 1994"
+echo "My name is $FIRST_NAME $LAST_NAME and my date of birth is $DOB"
